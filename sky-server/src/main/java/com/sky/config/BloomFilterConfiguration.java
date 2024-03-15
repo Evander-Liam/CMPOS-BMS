df876fcd01c68f454e6a9d105736967393491a74
Liam <leiyuan_liam@qq.com>
2024-03-15 20:10:39 +0800

    延迟布隆过滤器的获取

--- sky-server/src/main/java/com/sky/config/BloomFilterConfiguration.java
+++ sky-server/src/main/java/com/sky/config/BloomFilterConfiguration.java
@@ -5,7 +5,7 @@ import com.google.common.hash.Funnels;
 import com.sky.mapper.CategoryMapper;
 import com.sky.properties.BloomProperties;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,24 +15,25 @@ import java.util.List;
 /**
  * 配置类，用于创建BloomFilter对象
  */
-
 @Configuration
 @Slf4j
 public class BloomFilterConfiguration {
     @Bean
-    @ConditionalOnMissingBean
-    public BloomFilter bloomFilter(BloomProperties bloomProperties, CategoryMapper categoryMapper) {
-        BloomFilter bloomFilter = BloomFilter.create(Funnels.stringFunnel(Charset.defaultCharset())
+    public BloomFilter<String> bloomFilter(BloomProperties bloomProperties) {
+        return BloomFilter.create(Funnels.stringFunnel(Charset.defaultCharset())
                 , bloomProperties.getExpectedInsertions()
                 , bloomProperties.getFalsePositiveProbability());
+    }
 
-        log.info("开始初始化布隆过滤器");
-        List<Long> allCategoryIds = categoryMapper.getAllCategoryIds();
-        for (Long categoryId : allCategoryIds) {
-            bloomFilter.put("DishCache::" + categoryId);
-            bloomFilter.put("SetmealCache::" + categoryId);
-        }
-
-        return bloomFilter;
+    @Bean
+    public CommandLineRunner initBloomFilter(BloomFilter<String> bloomFilter, CategoryMapper categoryMapper) {
+        return args -> {
+            log.info("开始初始化布隆过滤器");
+            List<Long> allCategoryIds = categoryMapper.getAllCategoryIds();
+            for (Long categoryId : allCategoryIds) {
+                bloomFilter.put("DishCache::" + categoryId);
+                bloomFilter.put("SetmealCache::" + categoryId);
+            }
+        };
     }
 }
